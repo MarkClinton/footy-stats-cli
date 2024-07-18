79fb89c6c15eb1f935387d7dc141f8dc0054fd04
Mark <markc9309@yahoo.com>
2024-07-18 17:46:21 +0100

    feat: add starting point to app in run.py

--- src/main.py
+++ src/main.py
@@ -1,2 +1,9 @@
 # The main file that deals with the overall running of the app.
 # Instantiates a new APIClient to which we can call all endpoints
+
+from .apiclient import APIClient
+
+class Main():
+    
+    def __init__(self) -> None:
+        print("hello")
