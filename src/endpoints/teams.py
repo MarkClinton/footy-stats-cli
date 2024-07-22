9a71668354a5d08d97f5f0dbb523e568755c0b9e
Mark <markc9309@yahoo.com>
2024-07-22 14:41:59 +0100

    refactor: change date format from '-' to '/'

--- src/endpoints/teams.py
+++ src/endpoints/teams.py
@@ -27,7 +27,7 @@ class Teams(BaseEndPoint):
             result = f'{home_score}-{away_score}'
 
             format_date = datetime.strptime(m["utcDate"], '%Y-%m-%dT%H:%M:%SZ')
-            match_date = f'{format_date.day}-{format_date.month}-{format_date.year}'
+            match_date = f'{format_date.day}/{format_date.month}/{format_date.year}'
             winner = m["score"]["winner"].replace('_TEAM','')
             
             match = {
