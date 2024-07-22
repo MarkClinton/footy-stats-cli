c1ab1266eee70d9a0acca5e63075653b34715ffa
Mark <markc9309@yahoo.com>
2024-07-22 10:25:19 +0100

    feat: add get_competition_matches and clean_matches_list to competitions class

--- src/endpoints/competitions.py
+++ src/endpoints/competitions.py
@@ -32,6 +32,33 @@ class Competitions(BaseEndPoint):
         response = self.request(self.BASE_COMPETITIONS_RESOURCE, "scorers")
         return(self.clean_scorers_list(self.process_response(response, "scorers")))
     
+    def get_competition_matches(self):
+        response = self.request(self.BASE_COMPETITIONS_RESOURCE, "matches")
+        return(self.clean_matches_list(self.process_response(response, "matches")))
+    
+    @staticmethod
+    def clean_matches_list(match_data):
+        matches = []
+
+        for m in match_data:
+            home_score = m["score"]["fullTime"]["home"]
+            away_score = m["score"]["fullTime"]["away"]
+            result = f'{home_score}-{away_score}'
+
+            format_date = datetime.strptime(m["utcDate"], '%Y-%m-%dT%H:%M:%SZ')
+            match_date = f'{format_date.day}-{format_date.month}-{format_date.year}'
+            winner = m["score"]["winner"].replace('_TEAM','')
+            
+            match = {
+                "Date": match_date,
+                "Home": m["homeTeam"]["name"],
+                "Away": m["awayTeam"]["name"],
+                "Winner": winner,
+                "Result": result
+            }
+            matches.append(match)
+        return matches
+
     @staticmethod
     def clean_scorers_list(scorers_data):
         scorers = []
