7eb170f3da0799ac75f72fb7e1df10cc53c3fc5b
Mark <markc9309@yahoo.com>
2024-07-22 16:41:54 +0100

    test: add temporary print statement for every endpoint to test functinlaity

--- src/main.py
+++ src/main.py
@@ -19,6 +19,32 @@ class Main():
         seasons = client.competitions.get_competition_seasons()
         for i in seasons:
             print(i)
+        
+        print("\n---------------\n")
+
+        teams = client.competitions.get_competition_teams()
+        for i in teams:
+            print(i)
+
+        print("\n---------------\n")
+
+        standings = client.competitions.get_competition_standings()
+        for i in standings:
+            print(i)
+        
+        print("\n---------------\n")
+
+        scorers = client.competitions.get_competition_goalscorers()
+        for i in scorers:
+            print(i)
+
+        print("\n---------------\n")
+
+        comp_matches = client.competitions.get_competition_matches()
+        for i in comp_matches:
+            print(i)
+
+        print("\n---------------\n")
 
         matches = client.teams.get_teams_matches()
         for i in matches:
