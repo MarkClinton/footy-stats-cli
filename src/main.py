e7268bf5be4a1a751f54ed0f971578daf7526e1f
Mark <markc9309@yahoo.com>
2024-07-23 22:19:54 +0100

    feat: imported tabulate & simple_term_menu. Testing functionlaity in main()

--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@
 The main file that deals with the overall running of the app.
 Instantiates a new APIClient to which we can call all endpoints
 """
-
+from tabulate import tabulate
+from simple_term_menu import TerminalMenu
 from .apiclient import APIClient
 
 
@@ -16,48 +17,71 @@ class Main():
     
     def __init__(self):
 
-        client = APIClient("PL")
+        leagues = [
+            {"code": "PL", "name": "Premier League"},
+            {"code": "FL1", "name": "Ligue 1"},
+            {"code": "PD", "name": "La Liga"},
+            {"code": "BL1", "name": "Bundesliga"},
+            {"code": "SA", "name": "Serie A"},
+        ]
 
+        options = [league["name"] for league in leagues]
+        terminal_menu = TerminalMenu(options)
+        menu_entry_index = terminal_menu.show()
+        code = leagues[menu_entry_index]["code"]
+        print(f"You have selected {code}!")
+
+        client = APIClient(code)
+        seasons = client.competitions.get_competition_seasons()
+        print(seasons)
+        season_list = [season["Name"] for season in seasons]
+        terminal_menu = TerminalMenu(season_list)
+        s = terminal_menu.show()
+        e = seasons[s]["Year"]
+        print(f"You have selected {e}!")
+        client.season = e
+        self.team(client)
+
+
+
+
+    def season(self, client):
+        
         print("\n---------------\n")
         print("Seasons")
         print("\n---------------\n")
         seasons = client.competitions.get_competition_seasons()
-        for i in seasons:
-            print(i)
-        
-        print("\n---------------\n")
-        print("Teams")
-        print("\n---------------\n")
-        client.season = "2023"
-        teams = client.competitions.get_competition_teams()
-        for i in teams:
-            print(i)
+        print(tabulate(seasons, headers="keys", colalign=("left",), tablefmt="rounded_outline"))
 
+    def data(self, client):
         print("\n---------------\n")
         print("League Standings")
         print("\n---------------\n")
         standings = client.competitions.get_competition_standings()
-        for i in standings:
-            print(i)
+        print(tabulate(standings, headers="keys", colalign=("left",), tablefmt="rounded_outline"))
         
         print("\n---------------\n")
         print("Top 10 Goalscorers")
         print("\n---------------\n")
         scorers = client.competitions.get_competition_goalscorers()
-        for i in scorers:
-            print(i)
+        print(tabulate(scorers, headers="keys", colalign=("left",), tablefmt="rounded_outline"))
 
         print("\n---------------\n")
         print("All Competition Matches")
         print("\n---------------\n")
         comp_matches = client.competitions.get_competition_matches()
-        for i in comp_matches:
-            print(i)
+        print(tabulate(comp_matches, headers="keys", colalign=("left",), tablefmt="rounded_outline"))
 
+    def match(self, client):
         print("\n---------------\n")
         print("Team Matches (Team: Arsenal)")
         print("\n---------------\n")
-        client.team = 57
         matches = client.teams.get_teams_matches()
-        for i in matches:
-            print(i)
+        print(tabulate(matches, headers="keys", colalign=("left",), tablefmt="rounded_outline"))
+
+    def team(self, client):
+        print("\n---------------\n")
+        print("Teams")
+        print("\n---------------\n")
+        teams = client.competitions.get_competition_teams()
+        print(tabulate(teams, headers="keys", colalign=("left",), tablefmt="rounded_outline"))
