b2a2f02f413070eee3b7ba2d8b76d075e21d8cad
Mark <markc9309@yahoo.com>
2024-07-23 12:31:41 +0100

    test: add print statements to main.py to show what data is printed

--- src/main.py
+++ src/main.py
@@ -15,37 +15,47 @@ class Main():
     def __init__(self):
 
         client = APIClient("PL")
-        
+
+        print("\n---------------\n")
+        print("Seasons")
+        print("\n---------------\n")
         seasons = client.competitions.get_competition_seasons()
         for i in seasons:
             print(i)
         
         print("\n---------------\n")
-        client.season = "2022"
+        print("Teams")
+        print("\n---------------\n")
+        client.season = "2023"
         teams = client.competitions.get_competition_teams()
         for i in teams:
             print(i)
 
         print("\n---------------\n")
-
+        print("League Standings")
+        print("\n---------------\n")
         standings = client.competitions.get_competition_standings()
         for i in standings:
             print(i)
         
         print("\n---------------\n")
-
+        print("Top 10 Goalscorers")
+        print("\n---------------\n")
         scorers = client.competitions.get_competition_goalscorers()
         for i in scorers:
             print(i)
 
         print("\n---------------\n")
-        # client.league = "BSA"
+        print("All Competition Matches")
+        print("\n---------------\n")
         comp_matches = client.competitions.get_competition_matches()
         for i in comp_matches:
             print(i)
 
         print("\n---------------\n")
-        client.team = 1044
+        print("Team Matches (Team: Arsenal)")
+        print("\n---------------\n")
+        client.team = 57
         matches = client.teams.get_teams_matches()
         for i in matches:
             print(i)
