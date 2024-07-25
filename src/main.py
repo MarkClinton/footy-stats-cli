f09d79729ae282e3e2a6361a93e3732194e2aa13
Mark <markc9309@yahoo.com>
2024-07-25 09:29:54 +0100

    refactor: change Menu() back to mixin. Allow it to handle all menu activity.

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from .apiclient import APIClient
 from .menu import Menu
 
 
-class Main():
+class Main(Menu):
     """
     Main() class that handles the app logic. Displaying data, fetching data
     and showing menu items.
@@ -18,49 +18,24 @@ class Main():
     
     def __init__(self):
 
-        leagues = [
-            {"code": "PL", "name": "Premier League"},
-            {"code": "FL1", "name": "Ligue 1"},
-            {"code": "PD", "name": "La Liga"},
-            {"code": "BL1", "name": "Bundesliga"},
-            {"code": "SA", "name": "Serie A"},
-        ]
+        league_menu = self.menu(self.get_league_menu(), self.get_league_title())
+        league_sel = league_menu.show()
+        league = self.get_league_option(league_sel)
 
-        title = "  Main Menu.\n  Choose your League\n  Press Q or Esc to quit. \n"
-        items = [league["name"] for league in leagues]
-        exit = False
-        
-
-        menu = Menu(title, items)
-        main = menu.menu()
-        main_option = main.show()
-        
-        league_code = leagues[main_option]["code"]
-
-        client = APIClient(league_code)
+        client = APIClient(league)
         seasons = client.competitions.get_competition_seasons()
+        season_options = self.list_to_menu_options(seasons, "Name")
 
-        menu.main_menu_items = [season["Name"] for season in seasons]
-        menu.main_menu_title = "Leagues"
-        season = menu.menu()
-        season_option = season.show()
+        season_menu = self.menu(season_options, "Seasons")
+        season_sel = season_menu.show()
 
-        client.season = seasons[season_option]["Year"]
+        client_season = self.get_list_option(seasons, season_sel)
+        client.season = client_season
 
-        league_options = [
-            {"code": "comp_teams", "option": "Show All Teams"},
-            {"code": "comp_standings", "option": "Show League Table"},
-            {"code": "comp_matches", "option": "Show All Results"},
-            {"code": "teams_matches", "option": "Show a Teams Results"},
-            {"code": "comp_goalscorers", "option": "Show Top 10 Goalscorers"},
-        ]
+        main_menu = self.menu(self.get_main_menu(), self.get_main_title())
+        main_sel = main_menu.show()
 
-        menu.main_menu_items = [l_option["option"] for l_option in league_options]
-        menu.main_menu_title = "Leagues"
-        foo = menu.menu()
-        foo_option = foo.show()
-        
-        endpoint = league_options[foo_option]["code"]
+        endpoint = self.get_main_option(main_sel)
 
         if endpoint == "comp_teams":
             data = client.competitions.get_competition_teams()
--- src/menu.py
+++ src/menu.py
@@ -4,24 +4,58 @@ This class handles creating and displaying menu's for the end user
 from tabulate import tabulate
 from simple_term_menu import TerminalMenu
 
+
 class Menu():
 
-    def __init__(self, title, menu_items):
-        self.main_menu_title = title
-        self.main_menu_items = menu_items
-        self.main_menu_cursor = "> "
-        self.main_menu_cursor_style = ("fg_green", "bold")
-        self.main_menu_style = ("bg_green", "fg_yellow", "bold")
+    LEAGUES = [
+            {"code": "PL", "name": "Premier League"},
+            {"code": "FL1", "name": "Ligue 1"},
+            {"code": "PD", "name": "La Liga"},
+            {"code": "BL1", "name": "Bundesliga"},
+            {"code": "SA", "name": "Serie A"},
+        ]
+    
+    MAIN_MENU = [
+            {"code": "comp_teams", "option": "Show All Teams"},
+            {"code": "comp_standings", "option": "Show League Table"},
+            {"code": "comp_matches", "option": "Show All Results"},
+            {"code": "teams_matches", "option": "Show a Teams Results"},
+            {"code": "comp_goalscorers", "option": "Show Top 10 Goalscorers"},
+        ] 
 
-    def menu(self):
+    def menu(self, menu_options, title):
 
-        main_menu = TerminalMenu(
-            menu_entries = self.main_menu_items,
-            title = self.main_menu_title,
-            menu_cursor = self.main_menu_cursor,
-            menu_cursor_style = self.main_menu_cursor_style,
-            menu_highlight_style = self.main_menu_style,
+        menu = TerminalMenu(
+            menu_entries = menu_options,
+            title = title,
+            menu_cursor = "> ",
+            menu_cursor_style = ("fg_green", "bold"),
+            menu_highlight_style = ("bg_green", "fg_yellow", "bold"),
             cycle_cursor = True,
             clear_screen = True,
         )
-        return main_menu
+        return menu
+    
+    def get_league_menu(self):
+        return [league["name"] for league in self.LEAGUES]
+    
+    def get_league_title(self):
+        return "Leagues"
+    
+    def get_league_option(self, pos):
+        return self.LEAGUES[pos]["code"]
+
+    def get_main_menu(self):
+        return [m["option"] for m in self.MAIN_MENU]
+
+    def get_main_title(self):
+        return "Main Menu"
+    
+    def get_main_option(self, pos):
+        return self.MAIN_MENU[pos]["code"]
+    
+    def list_to_menu_options(self, data, k):
+        return [d[k] for d in data]
+    
+    def get_list_option(self, data, pos):
+        return data[pos]["Year"]
