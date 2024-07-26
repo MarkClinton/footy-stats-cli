33937182494ad04818d88b0f8d6d3a99ba7c6896
Mark <markc9309@yahoo.com>
2024-07-26 10:41:53 +0100

    refactor: clean up clean_matches_list function return dict

--- src/endpoints/mixin.py
+++ src/endpoints/mixin.py
@@ -17,20 +17,21 @@ class Mixin():
         matches = []
 
         for m in match_data:
+
+            home_team = m["homeTeam"]["name"]
             home_score = m["score"]["fullTime"]["home"]
+            away_team = m["awayTeam"]["name"]
             away_score = m["score"]["fullTime"]["away"]
-            result = f'{home_score}-{away_score}'
+            score = f'{home_score}-{away_score}'
 
             format_date = datetime.strptime(m["utcDate"], '%Y-%m-%dT%H:%M:%SZ')
             match_date = f'{format_date.day}/{format_date.month}/{format_date.year}'
-            winner = m["score"]["winner"].replace('_TEAM','')
             
             match = {
                 "Date": match_date,
-                "Home": m["homeTeam"]["name"],
-                "Away": m["awayTeam"]["name"],
-                "Winner": winner,
-                "Result": result
+                "Home": home_team,
+                "Score": score,
+                "Away": away_team
             }
             matches.append(match)
         return matches
