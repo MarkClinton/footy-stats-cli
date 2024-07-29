a86e2314131f3bbf3a1a59992c5ddf39acb73929
Mark <markc9309@yahoo.com>
2024-07-29 09:28:45 +0100

    refactor: update file to adhere to pep8 standards

--- src/endpoints/teams.py
+++ src/endpoints/teams.py
@@ -1,4 +1,4 @@
-# This is a class that will get the Team data 
+# This is a class that will get the Team data
 # Only deals with the info needed to call the team endpoint
 
 from .baseendpoint import BaseEndPoint
@@ -7,16 +7,16 @@ from .endpointutil import EndpointUtil
 
 class Teams(BaseEndPoint, EndpointUtil):
     """
-    Teams() class handles fetching all Teams data. It inherits BaseEndPoint as 
+    Teams() class handles fetching all Teams data. It inherits BaseEndPoint as
     the parent to call the neccessary request functions.
     """
-    
+
     BASE_TEAMS_RESOURCE = "teams"
 
     def get_teams_matches(self) -> list:
         """ Get a list of all matches a team played in a given season """
 
         response = self.request(self.BASE_TEAMS_RESOURCE, "matches")
-        return(
+        return (
             self.clean_matches_list(self.process_response(response, "matches"))
         )
