3f4a4c25a221f9bf3feb9b59acc3fd826e613dd2
Mark <markc9309@yahoo.com>
2024-07-29 09:33:32 +0100

    refactor: change comment to docstring

--- src/endpoints/baseendpoint.py
+++ src/endpoints/baseendpoint.py
@@ -1,8 +1,10 @@
-# This file will contain a request method to deal with getting the data and a
-# process_response to deal with formatting the data. It can be used by the
-# endpoint classes i.e competitions.request() or competitions.process_request()
-# Deals with the getting and processing of the data
-# It uses BaseClient as the parent class to get headers for the request
+"""
+This file contains a request method to deal with getting the data and a
+process_response to deal with formatting the data. It can be used by the
+endpoint classes i.e competitions.request() or competitions.process_request()
+Deals with the getting and processing of the data
+It uses BaseClient as the parent class to get headers for the request
+"""
 
 import requests
 from ..baseclient import BaseClient
