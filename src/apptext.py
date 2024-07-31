504d638cd3cf3aefec39637b726f733b584beddc
Mark <markc9309@yahoo.com>
2024-07-31 16:24:17 +0100

    refactor: update and clean the AppText file

--- src/apptext.py
+++ src/apptext.py
@@ -9,64 +9,64 @@ class AppText():
     """
     term = Terminal()
 
+    PROCESSING = f'{term.blink("Processing Request...")}'
+    ANY_KEY = f'{term.bold_yellow}[ANY KEY]{term.normal}'
+    Q = f'{term.bold_yellow}[Q]{term.normal}'
+    GREEN = f'{term.bold_lightgreen}'
+    NORMAL = f'{term.normal}\n'
+    UP_DOWN = f'{term.bold_yellow}[Up/Down]{term.normal}'
+    ENTER = f'{term.bold_yellow}[Enter]{term.normal}'
+    ESC = f'{term.bold_yellow}[Q/ESC]{term.normal}'
+    MAIN_TITLE = (f"""Select the option you want to view data for below:""")
     LEAGUE_TITLE = (f"""Select the league you want to view data for below:""")
+    SEASON_TITLE = (f"""Select the season you want to view data for below:""")
+    TEAM_TITLE = (f"""Select a team below to view their fixtures/results:""")
+    START_TITLE = (f"""Select an option below:""")
 
     LEAGUE_ABOUT = (f"""
 Footy Stats CLI has 5 League options to choose from.
-Click {term.bold_lightgreen}[Q/ESC]{term.normal} to go back to start menu.
+Click {ESC} to go back to start menu.
                     """)
 
     MAIN_ABOUT = (f"""
 Footy Stats CLI has 5 main options to choose from. 
-Click {term.bold_lightgreen}[Q/ESC]{term.normal} to go back to season menu.
+Click {ESC} to go back to season menu.
                 """)
-    MAIN_TITLE = (f"""Select the option you want to view data for below:""")
 
     SEASON_ABOUT = (f"""
 Footy Stats CLI shows up to 5 seasons worth of historical data.
-Click {term.bold_lightgreen}[Q/ESC]{term.normal} to go back to league menu.
+Click {ESC} to go back to league menu.
                     """)
-    SEASON_TITLE = (f"""Select the season you want to view data for below:""")
 
     TEAM_ABOUT = (f"""
 TEAM MENU
 Below shows the full list of available teams.
 Click [Q/ESC] to go back to main menu.
                 """)
-    TEAM_TITLE = (f"""Select a team below to view their fixtures/results:""")
 
     START_ABOUT = (f"""
 Footy Stats CLI is an application for all football related data. Using the
 Football-Data API to gather data about Leagues, Fixtures/Results, Teams
 and Goalscorers.
 
-Navigate the app using the keyboard.{term.bold_lightgreen}[Up/Down]{term.normal} moves through menu options.
-{term.bold_lightgreen}[Enter]{term.normal} selects the option. Click the Help option below for more info.
-Click {term.bold_lightgreen}[Q/ESC]{term.normal} to exit the app.
+Navigate the app using the keyboard.
+{UP_DOWN} moves through menu options. {ENTER} selects the option. 
+Click the Help option below for more info.
+Click {term.bold_yellow}[Q/ESC]{term.normal} to exit the app.
                 """)
-    
-    START_TITLE = (f"""Select an option below:""")
-
-    LOGO = (f"""{term.bold_lightgreen}
-   ____        __         ______       __        _______   ____
-  / ___/_ ___ / /___ __  / __/ /____ _/ /____   / ___/ /  /  _/
- / _// _ / _ / __/ // / _\ \/ __/ _ `/ __(_-<  / /__/ /___/ /  
-/_/  \___\___\__/\_, / /___/\__/\_,_/\__/___/  \___/____/___/  
-                /___/ {term.normal}
-            """)
 
     HELP_MESSAGE = (f"""
 Footy Stats CLI uses the keyboard to navigate menu options. See the
 cheatsheet below on how to use Footy Stats CLI:
 
-{term.bold_yellow}[UP/DOWN]{term.normal}: Navigate menu items.
-{term.bold_yellow}[ENTER]{term.normal}: Select the option and either display 
+{UP_DOWN}: Navigate menu items.
+{ENTER}: Select the option and either display 
 the data you have chosen or progress to the next menu.
-{term.bold_yellow}[Q/ESC]{term.normal}: Click [Q] or [ESC] on the keyboard to 
+{ESC}: Click [Q] or [ESC] on the keyboard to 
 either go back to the previous menu or exit the program 
 depending on what menu screen you are on. Clicking [Q] or 
 [ESC] on the Start Menu screen will exit the program.
-{term.bold_yellow}[ANY KEY]{term.normal}: Progress the app
+{ANY_KEY}: Progress the app when prompted.
                 """)
 
     ABOUT_MESSAGE = (f"""
@@ -81,9 +81,11 @@ Seasons: From 2020 onwards.
 Data: Teams, League Table, Fixtures & Results, Top 10 Goalscorers
                     """)
     
-    PROCESSING = f'{term.blink("Processing Request...")}'
+    LOGO = (f"""{term.bold_lightgreen}
+   ____        __         ______       __        _______   ____
+  / ___/_ ___ / /___ __  / __/ /____ _/ /____   / ___/ /  /  _/
+ / _// _ / _ / __/ // / _\ \/ __/ _ `/ __(_-<  / /__/ /___/ /  
+/_/  \___\___\__/\_, / /___/\__/\_,_/\__/___/  \___/____/___/  
+                /___/ {term.normal}
+            """)
 
-    ANY_KEY = f'{term.bold_lightgreen}[ANY KEY]{term.normal}'
-    Q = f'{term.bold_lightgreen}[Q]{term.normal}'
-    GREEN = f'{term.bold_lightgreen}'
-    NORMAL = f'{term.normal}\n'
